107b2119ab44dcd8cefc5ef3884acea94766d9d0
Mmy2000 <yahianada005@gmail.com>
2024-09-12 21:42:14 +0300

    create footer

---
+++ src/components/HeaderSlider.jsx
@@ -0,0 +1,21 @@
+// HeaderSlider.jsx
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import bg1 from "../images/blog-1.jpg";
+import bg2 from "../images/bg2.jpg";
+import bg3 from "../images/bg3.jpg";
+import bg4 from "../images/bg4.jpg";
+
+
+export default function HeaderSlider() {
+  
+  
+  return (
+    <div>
+      <h2>Home page</h2>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from "react";
+import HeaderSlider from "../components/HeaderSlider";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>Home Page</div>
-  )
+    <HeaderSlider/>
+  );
 }
