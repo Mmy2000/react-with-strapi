b54f827fd9e61d810c5d2836b2ee0c4b456667b6
Mmy2000 <yahianada005@gmail.com>
2024-09-12 19:52:29 +0300

    create navbar and router

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Route , Routes } from 'react-router-dom'
+import { createBrowserRouter, Route , RouterProvider, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import Product from './pages/Product'
+import Layout from './pages/Layout'
+import { QueryClient, QueryClientProvider } from "react-query";
 
+let router = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products/>,
+      },
+      {
+        path: "products/:id",
+        element: <Product/>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
+const queryClient = new QueryClient();
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/products" element={<Products />}></Route>
-        <Route path="products/:id" element={<Product />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from "@chakra-ui/react";
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
 
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <Router>
+  
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </Router>
-  </QueryClientProvider>
+    
 );
