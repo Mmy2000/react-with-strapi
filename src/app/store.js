7ba3aae64ed48781ccb17f17035258ef1f2659a4
Mmy2000 <yahianada005@gmail.com>
2024-10-02 04:32:44 +0300

    store the cartitem in local storage

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './features/loginSlice'; // Import the reducer instead
 import cartSlice from "./features/cartSlice"
 import globalSlice from "./features/globalSlice"
+import { persistStore,persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"
 
+
+const persistCartConfig = {
+  key:'cart',
+  storage
+}
+
+const persistedCart = persistReducer(persistCartConfig,cartSlice)
 
 export const store = configureStore({
   reducer: {
-    cart:cartSlice,
+    cart:persistedCart,
     login: loginReducer, // Use the imported reducer
     global:globalSlice
   },
 });
+
+export const persistor = persistStore(store)
