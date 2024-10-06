dd8b57838e3eef04de0d3af6ab288195db774b5a
Mmy2000 <yahianada005@gmail.com>
2024-10-06 20:56:53 +0300

    add skeleton

--- src/components/DashboardSceleton.jsx
+++ src/components/DashboardSceleton.jsx
@@ -9,60 +9,84 @@ import {
   Td,
   Tfoot,
   Skeleton,
+  HStack,
+  Box,
+  SkeletonText,
+  Stack,
 } from "@chakra-ui/react";
 
 const DashboardSkeleton = () => {
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {/* First Row Skeleton */}
-          <Tr>
-            <Td>
-              <Skeleton height="20px" />
-            </Td>
-            <Td>
-              <Skeleton height="20px" />
-            </Td>
-            <Td isNumeric>
-              <Skeleton height="20px" />
-            </Td>
-          </Tr>
-          {/* Second Row Skeleton */}
-          <Tr>
-            <Td>
-              <Skeleton height="20px" />
-            </Td>
-            <Td>
-              <Skeleton height="20px" />
-            </Td>
-            <Td isNumeric>
-              <Skeleton height="20px" />
-            </Td>
-          </Tr>
-          {/* Third Row Skeleton */}
-          <Tr>
-            <Td>
-              <Skeleton height="20px" />
-            </Td>
-            <Td>
-              <Skeleton height="20px" />
-            </Td>
-            <Td isNumeric>
-              <Skeleton height="20px" />
-            </Td>
-          </Tr>
-        </Tbody>
-        
-      </Table>
-    </TableContainer>
+    <Box w="full" p={4}>
+      <TableContainer w="full">
+        <Table variant="simple" w="full">
+          <Thead>
+            <Tr>
+              <Th>
+                <Skeleton height="20px" width="80px" />
+              </Th>
+              <Th>
+                <Skeleton height="20px" width="80px" />
+              </Th>
+              <Th isNumeric>
+                <Skeleton height="20px" width="100px" />
+              </Th>
+              <Th>
+                <Skeleton height="20px" width="80px" />
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {/* Repeat this structure for multiple rows */}
+            {[...Array(5)].map((_, index) => (
+              <Tr key={index}>
+                <Td>
+                  <Skeleton height="20px" />
+                </Td>
+                <Td>
+                  <Skeleton height="20px" />
+                </Td>
+                <Td isNumeric>
+                  <Skeleton height="20px" />
+                </Td>
+                <Td>
+                  <HStack justifyContent="flex-end">
+                    <Skeleton
+                      startColor="yellow.100"
+                      endColor="yellow.400"
+                      height="20px"
+                      width="60px"
+                    />
+                    <Skeleton
+                      startColor="red.100"
+                      endColor="red.400"
+                      height="20px"
+                      width="60px"
+                    />
+                  </HStack>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>
+                <Skeleton height="20px" width="80px" />
+              </Th>
+              <Th>
+                <Skeleton height="20px" width="80px" />
+              </Th>
+              <Th isNumeric>
+                <Skeleton height="20px" width="100px" />
+              </Th>
+              <Th>
+                <Skeleton height="20px" width="80px" />
+              </Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 
