c584fe8f5d548b56c435eaadbaa00e3088c3de3f
Mmy2000 <yahianada005@gmail.com>
2024-10-07 19:49:18 +0300

    create alert dailog

--- src/components/DashboardProducts.jsx
+++ src/components/DashboardProducts.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   TableContainer,
   Image,
@@ -11,6 +10,7 @@ import {
   Tfoot,
   Td,
   Button,
+  useDisclosure,
 } from "@chakra-ui/react";
 import DashboardSkeleton from "./DashboardSceleton";
 import { useGetDashboardDataQuery } from "../app/services/apiSlice";
@@ -19,9 +19,12 @@ import { AiOutlineEye } from "react-icons/ai";
 import { BsTrash } from "react-icons/bs";
 import { FiEdit2 } from "react-icons/fi";
 import { formatPrice } from "../utils/functions";
+import CustomeAlertDailog from "../shared/AlertDailog";
+
 
 
 const DashboardProducts = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { isLoading, data, error } = useGetDashboardDataQuery({ page: 1 });
   console.log(data);
 
@@ -29,87 +32,90 @@ const DashboardProducts = () => {
     return <DashboardSkeleton />;
   }
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>ID</Th>
-            <Th>Title</Th>
-            <Th>Category</Th>
-            <Th>Thumbnail</Th>
-            <Th>Price</Th>
-            <Th>Stock</Th>
-            <Th>Action</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data?.data?.map((product) => (
-            <Tr key={product.id}>
-              <Td>{product.id}</Td>
-              <Td>{product?.attributes.title}</Td>
-              <Td>
-                {product?.attributes?.categories?.data[0]?.attributes?.title}
-              </Td>
-              <Td>
-                <Image
-                  borderRadius="full"
-                  objectFit={"cover"}
-                  boxSize="40px"
-                  src={`${import.meta.env.VITE_SERVER_URL}${
-                    product?.attributes?.thumbnail?.data?.attributes?.formats
-                      ?.thumbnail?.url
-                  }`}
-                />
-              </Td>
-              <Td>${formatPrice(product.attributes.price)}</Td>
-              <Td>{product.attributes.stock}</Td>
-              <Td>
-                <Button
-                  as={Link}
-                  to={`/products/${product.id}`}
-                  colorScheme="purple"
-                  variant={"solid"}
-                  mr={3}
-                >
-                  <AiOutlineEye size={17} />
-                </Button>
-
-                <Button
-                  mr={3}
-                  onClick={() =>
-                    console.log(`update ${product.attributes.title}`)
-                  }
-                  colorScheme="blue"
-                  variant={"solid"}
-                >
-                  <FiEdit2 />
-                </Button>
-                <Button
-                  onClick={() =>
-                    console.log(`delete ${product.attributes.title}`)
-                  }
-                  colorScheme="red"
-                  variant={"solid"}
-                >
-                  <BsTrash size={17} />
-                </Button>
-              </Td>
+    <>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Title</Th>
+              <Th>Category</Th>
+              <Th>Thumbnail</Th>
+              <Th>Price</Th>
+              <Th>Stock</Th>
+              <Th>Action</Th>
             </Tr>
-          ))}
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>ID</Th>
-            <Th>Title</Th>
-            <Th>Category</Th>
-            <Th>Thumbnail</Th>
-            <Th>Price</Th>
-            <Th>Stock</Th>
-            <Th>Action</Th>
-          </Tr>
-        </Tfoot>
-      </Table>
-    </TableContainer>
+          </Thead>
+          <Tbody>
+            {data?.data?.map((product) => (
+              <Tr key={product.id}>
+                <Td>{product.id}</Td>
+                <Td>{product?.attributes.title}</Td>
+                <Td>
+                  {product?.attributes?.categories?.data[0]?.attributes?.title}
+                </Td>
+                <Td>
+                  <Image
+                    borderRadius="full"
+                    objectFit={"cover"}
+                    boxSize="40px"
+                    src={`${import.meta.env.VITE_SERVER_URL}${
+                      product?.attributes?.thumbnail?.data?.attributes?.formats
+                        ?.thumbnail?.url
+                    }`}
+                  />
+                </Td>
+                <Td>${formatPrice(product.attributes.price)}</Td>
+                <Td>{product.attributes.stock}</Td>
+                <Td>
+                  <Button
+                    as={Link}
+                    to={`/products/${product.id}`}
+                    colorScheme="purple"
+                    variant={"solid"}
+                    mr={3}
+                  >
+                    <AiOutlineEye size={17} />
+                  </Button>
+
+                  <Button
+                    mr={3}
+                    onClick={() =>
+                      console.log(`update ${product.attributes.title}`)
+                    }
+                    colorScheme="blue"
+                    variant={"solid"}
+                  >
+                    <FiEdit2 />
+                  </Button>
+                  <Button
+                    onClick={
+                      onOpen
+                    }
+                    colorScheme="red"
+                    variant={"solid"}
+                  >
+                    <BsTrash size={17} />
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Title</Th>
+              <Th>Category</Th>
+              <Th>Thumbnail</Th>
+              <Th>Price</Th>
+              <Th>Stock</Th>
+              <Th>Action</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+      <CustomeAlertDailog isOpen={isOpen} onOpen={onOpen} onClose={onClose}/>
+    </>
   );
 };
 
