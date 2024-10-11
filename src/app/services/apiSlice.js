21f8524b2e6aa46ba6a25ead84858ac9cb11c103
Mmy2000 <yahianada005@gmail.com>
2024-10-11 04:23:35 +0300

    send data to backend using form data

--- src/app/services/apiSlice.js
+++ src/app/services/apiSlice.js
@@ -1,39 +1,78 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import CookieService from '../../pages/CookieService';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import CookieService from "../../pages/CookieService";
 
 export const apiSlice = createApi({
-    reducerPath: 'api',
-    tagTypes: ['Products'],
-    refetchOnReconnect:true,
-    refetchOnMountOrArgChange:true,
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_SERVER_URL }),
-    endpoints: (build) => ({
-        getDashboardData: build.query({
-            query: (arg) => {
-                const { page} = arg;
-                return {
-                    url: `/api/products?populate=thumbnail,categories&pagination[page]=${page}&pagination[pageSize]=7`,
-                };
-            },
-            providesTags: (result, error, arg) =>
+  reducerPath: "api",
+  tagTypes: ["Products"],
+  refetchOnReconnect: true,
+  refetchOnMountOrArgChange: true,
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_SERVER_URL,
+  }),
+  endpoints: (build) => ({
+    // get data
+    getDashboardData: build.query({
+      query: (arg) => {
+        const { page } = arg;
+        return {
+          url: `/api/products?populate=thumbnail,categories&pagination[page]=${page}&pagination[pageSize]=7`,
+        };
+      },
+      providesTags: (result, error, arg) =>
         result
-          ? [...result.data.map(({ id }) => ({ type: 'Products', id })), 'Products']
-          : ['Products'],
-        }),
-        
-        deleteDashboardProduct:build.mutation({
-            query(id){
-                return {
-                    url:`/api/products/${id}`,
-                    method:'DELETE',
-                    headers:{
-                        Authorization: `Bearer ${CookieService.get('jwt')}`
-                    }
-                }
-            },
-            invalidatesTags: ['Products'],
-        })
+          ? [
+              ...result.data.map(({ id }) => ({
+                type: "Products",
+                id,
+              })),
+              "Products",
+            ]
+          : ["Products"],
     }),
+    // DELETE
+    deleteDashboardProduct: build.mutation({
+      query(id) {
+        return {
+          url: `/api/products/${id}`,
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${CookieService.get("jwt")}`,
+          },
+        };
+      },
+      invalidatesTags: ["Products"],
+    }),
+    // UPDATE
+
+    updateDashboardProduct: build.mutation({
+      query: ({ id, body }) => ({
+        url: `/api/products/${id}`,
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${CookieService.get("jwt")}`,
+        },
+        body,
+      }),
+      async onQueryStarted({ id, ...patch }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData(
+            "getDashboardData",
+            id,
+            (draft) => {
+              Object.assign(draft, patch);
+            }
+          )
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: ["Products"],
+    }),
+  }),
 });
 
-export const { useGetDashboardDataQuery,useDeleteDashboardProductMutation } = apiSlice;
+export const { useGetDashboardDataQuery, useDeleteDashboardProductMutation,useUpdateDashboardProductMutation } =
+  apiSlice;
